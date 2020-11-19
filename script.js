d4cec237082ba19a5b89acbd509ca41e05aa6d91
aditi9012 <aditi.1721it1011@kiet.edu>
2020-11-19 11:19:48 +0530

    added js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
+    console.log('yes');
     let billAmount = document.getElementById('bill-amount').value;
     let tipPercentage = document.getElementById('tip-percentage').value;
-
+    let peopleAmount=document.getElementById('tip-amount').value;
     
     let tipAmount = document.getElementById('tip-amount').value = billAmount / tipPercentage;
-    document.getElementById('total-bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+   if(billAmount){
+       return true;
+   }
+   else{
+       alert('enter bill amount');
+   }
+   document.getElementById('total-tip').value=parseFloat(tipAmount)/peopleAmount;
+    
+    document.getElementById('total-bill').value = (parseFloat(billAmount) + parseFloat(tipAmount))/peopleAmount;
 
 })
